9e2fa6115013736ce14ded722ba0dc5883bc0c48
Jesse Heikkilä <jessevheikkila@gmail.com>
2019-05-11 22:00:25 +0300

    Add snap scrolling

--- src/components/Home/Desktop/index.tsx
+++ src/components/Home/Desktop/index.tsx
@@ -4,17 +4,38 @@ import TopLayout from './TopLayout'
 import MidLayout from './MidLayout'
 import BottomLayout from './BottomLayout'
 import { renderDesktopOnly } from 'components/resolutionRenderHOCs'
+import ReactFullpage from '@fullpage/react-fullpage'
 
-class HomePageDesktop extends React.PureComponent<{}, {}> {
-  public render() {
-    return (
-      <Layout>
-        <TopLayout />
-        <MidLayout />
-        <BottomLayout />
-      </Layout>
-    )
-  }
-}
+const HomePageDesktop = () => (
+  <ReactFullpage
+    render={() => (
+      <ReactFullpage.Wrapper>
+        <Layout>
+          <div className="section">
+            <TopLayout />
+          </div>
+          <div className="section">
+            <MidLayout />
+          </div>
+          <div className="section">
+            <BottomLayout />
+          </div>
+        </Layout>
+      </ReactFullpage.Wrapper>
+    )}
+  />
+)
+
+// class HomePageDesktop extends React.PureComponent<{}, {}> {
+//   public render() {
+//     return (
+//       <Layout>
+//         <TopLayout />
+//         <MidLayout />
+//         <BottomLayout />
+//       </Layout>
+//     )
+//   }
+// }
 
 export default renderDesktopOnly(HomePageDesktop)
