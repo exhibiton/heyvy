3d35b84e31c1b2c98c3993557f8e6569be176404
Jesse Heikkilä <jessevheikkila@gmail.com>
2019-05-22 13:08:49 +0300

    make contact to be a stateless component and clean it up

--- src/components/ContantForm/index.tsx
+++ src/components/ContantForm/index.tsx
@@ -1,35 +1,14 @@
 import { StyledForm, StyledInput, StyledSubmit } from './formStyles'
 import React from 'react'
-import axios from 'axios'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-
-interface State {
-  email: string
-  isSubmitting: boolean
-  message: string
-  name: string
-}
-
-class EmailInput extends React.Component<{}, State> {
-  public state: State = {
-    email: '',
-    isSubmitting: false,
-    message: '',
-    name: '',
-  }
-
+class EmailInput extends React.PureComponent<{}, {}> {
   public render() {
     return (
-      <>
-        <ToastContainer />
-        <StyledForm name="contact" method="POST" data-netlify="true">
-          <StyledInput type="text" name="name" placeholder="Name *" />
-          <StyledInput type="text" name="email" placeholder="Email address *" />
-          <StyledInput type="text" name="message" placeholder="Message *" />
-          <StyledSubmit type="submit">Request Quote</StyledSubmit>
-        </StyledForm>
-      </>
+      <StyledForm name="contact" method="POST" data-netlify="true">
+        <StyledInput type="text" name="name" placeholder="Name *" />
+        <StyledInput type="text" name="email" placeholder="Email address *" />
+        <StyledInput type="text" name="message" placeholder="Message *" />
+        <StyledSubmit type="submit">Request Quote</StyledSubmit>
+      </StyledForm>
     )
   }
 }
