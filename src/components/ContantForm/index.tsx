6b84049c11c13735e46b6a4c9f1dfa913679cf01
Jesse Heikkilä <jessevheikkila@gmail.com>
2019-05-23 18:41:11 +0300

    remove all styled components to try to fix netlify bug

--- src/components/ContantForm/index.tsx
+++ src/components/ContantForm/index.tsx
@@ -1,14 +1,15 @@
-import { StyledForm, StyledSubmit } from './formStyles'
 import React from 'react'
 class EmailInput extends React.PureComponent<{}, {}> {
   public render() {
     return (
-      <StyledForm name="contact" method="POST" data-netlify="true">
+      <form className="custom-form" name="contact" method="POST" data-netlify="true">
         <input className="custom-input" type="text" name="name" placeholder="Name *" />
         <input className="custom-input" type="text" name="email" placeholder="Email address *" />
         <input className="custom-input" type="text" name="message" placeholder="Message *" />
-        <StyledSubmit type="submit">Request Quote</StyledSubmit>
-      </StyledForm>
+        <button className="submit-btn" type="submit">
+          Request Quote
+        </button>
+      </form>
     )
   }
 }
